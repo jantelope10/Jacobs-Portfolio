a06874c5ffa3a9b5ee1fa359004ed2dc422df8d3
Jacob Sanchez <jantelope.10@gmail.com>
2025-08-28 21:16:14 -0700

    test

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
         }, 500);
         setTimeout(() => {
             elem.style.display = "none";
-            $(".all").style("filter", "none");
+            $(".all").css("filter", "blur(0px)");
         }, 600);
     });
     /*Shows spinner and blurs background when portfolio link in navbar is clicked, loads portfolio page after a timeout.*/
